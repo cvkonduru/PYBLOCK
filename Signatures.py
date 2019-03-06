3e56c6814b2a8e18115fbe1e4831f54bd56c536a
cvkonduru <cvkonduru@gmail.com>
2019-03-06 17:33:26 -0500

    KeyGen, DigSignature and Verification

---
+++ Signatures.py
@@ -0,0 +1,64 @@
+
+"""
+This Module implements functions to
+1) Generate Private and Public Keys
+2) Sign Messages
+3) Verify the Sinatures
+
+"""
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.exceptions import InvalidSignature
+
+# Function to Generate Private and Public Keys.
+
+
+def Generate_Keys():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+    public_key = private_key.public_key()
+
+    return private_key, public_key
+
+# Function to Sign a Message using Private Key.
+
+
+def Sign(message, private_key):
+    signature = private_key.sign(
+        message,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+
+    return signature
+
+# Fucntion to verify the signature.
+
+
+def Verify(message, signature, public_key):
+    try:
+        public_key.verify(
+            signature,
+            message,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return True
+
+    except InvalidSignature:
+        return False
+
+    except:
+        print("Error verifying ......!")
